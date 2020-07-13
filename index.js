8cdca19f83f1f117498cc2fd0fd22d738d2daa17
shubhamkumar96 <shubhamkumarranu@gmail.com>
2020-07-13 18:09:58 +0530

    Adding SignUp Validation Logic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const usersRepo = require("./repositories/users");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,8 +18,18 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email Already in Use");
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send("Passwords must match.");
+  }
+
   res.send("Account Created!!!");
 });
 
